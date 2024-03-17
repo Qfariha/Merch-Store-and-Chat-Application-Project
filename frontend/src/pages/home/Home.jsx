28d09b87a6700eee8556ac284b44a1f196a9e610
Qfariha <91182471+Qfariha@users.noreply.github.com>
2024-03-17 21:51:49 +0600

    First commit

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+	  <div className="bg-blue-500">
+		<h1 className="btn btn-ghost text-4xl font-mono text-red-500">Welcome!</h1>
+	  </div>
+	);
+  }
+  
+  export default Home;
